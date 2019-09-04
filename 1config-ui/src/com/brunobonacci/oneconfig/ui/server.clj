8add5b778eac22179c44a1aa5b9e4491e9b851ba
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-09-04 20:36:15 +0100

    added new REST layer for backend

--- 1config-ui/src/com/brunobonacci/oneconfig/ui/server.clj
+++ 1config-ui/src/com/brunobonacci/oneconfig/ui/server.clj
@@ -18,6 +18,7 @@
             [com.brunobonacci.oneconfig.backends :as b]
             [com.brunobonacci.oneconfig.util :as util]
             [clojure.string :as string]
+            [where.core :refer [where]]
             [taoensso.sente.server-adapters.http-kit
              :refer (sente-web-server-adapter)]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
@@ -33,9 +34,25 @@
   (util/default-backend-name))
 (def server-state (atom {:likes 0}))
 
+;;
+;; 1Config backend used by the UI-server
+;;
+(def backend (b/backend-factory {:type backend-name}))
+
+
+(defn- normalize
+  [params]
+
+  (cond-> params
+
+    (contains? params :change-num)
+    (update :change-num #(Long/parseLong %))))
+
+
 (defn ping-response [req]
   (println (str "pinging back" req) )
   {:status 200})
+
 
 (defroutes endpoints
            (POST "/oneconfig-add-entry"  {:keys [headers params body] :as request}
@@ -71,8 +88,46 @@
 
            (not-found "Not Found"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                            ;;
+;;                     ----==| N E W   L A Y E R |==----                      ;;
+;;                                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (GET "/configs" {{:keys [change-num] :as params} :params}
+       (let [filters (dissoc params :change-num)
+             change-num (when change-num (Long/parseLong change-num))
+             results (oneconfig/list backend filters)
+             ;; filtering further the result
+             results (if change-num
+                       (filter (where :change-num = change-num) results)
+                       results)]
+         (response results)))
+
+
+  (GET "/configs/keys/:key/envs/:env/versions/:version"
+       {:keys [params]}
+
+       (let [entry  (if (:change-num params)
+                      (oneconfig/load backend (normalize params))
+                      (oneconfig/find backend params))]
+
+         (if entry
+           (response entry)
+           (not-found "Config entry not found"))))
+
+
+
+  (POST "/configs" {:keys [body]}
+        (oneconfig/save backend body)
+        (response "ok"))
+
+
+  (not-found "Not Found"))
+
+
 (def handler
-  (-> (hdr/site endpoints)
+  (-> (hdr/site #'endpoints)
       (wrap-resource "public")
       (wrap-defaults
         (->
@@ -89,3 +144,19 @@
   (println (str "Server started: http://127.0.0.1:" PORT "/index.html"))
   (start-server :bind "0.0.0.0" :port NREPL-PORT)
   (println (str "nRepl server started: `lein repl :connect " NREPL-PORT "`")))
+
+
+
+(comment
+
+  (System/setProperty "aws.accessKeyId" "")
+  (System/setProperty "aws.secretKey"   "")
+  (System/setProperty "aws.region" "")
+
+
+  ;; start server for repl
+  (def s (http-kit/run-server #'handler {:port PORT}))
+
+  ;; stop server
+  (s)
+  )
